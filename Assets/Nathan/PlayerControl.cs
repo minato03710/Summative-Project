f752949efc06f92f0f40903174146c2a54644576
Crasher-EX <nathan@hay.kiwi>
2026-09-07 13:02:54 +1200

    b

--- Assets/Nathan/PlayerControl.cs
+++ Assets/Nathan/PlayerControl.cs
@@ -1,7 +1,15 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
 
 public class PlayerControl : MonoBehaviour
 {
+    public CharacterController characterController;
+    public float walkSpeed;
+    public float sprintSpeed;
+    public float jumpPower;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +19,21 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+
+    void Jump()
+    {
+
+    }
+
+    void Move()
+    {
+        float horizontalMove = Input.GetAxis("Horizontal");
+        float verticalMove = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.forward * verticalMove + transform.right * horizontalMove;
+        characterController.Move(walkSpeed * Time.deltaTime * move);
     }
 }
