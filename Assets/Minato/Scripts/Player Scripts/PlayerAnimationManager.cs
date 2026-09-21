6d8dee43f2a081f465c0d37d6c1e56f518d5caef
minato03710 <shimojihana1@gmail.com>
2026-09-21 12:53:19 +1200

    Player animation manager update continued

--- Assets/Minato/Scripts/Player Scripts/PlayerAnimationManager.cs
+++ Assets/Minato/Scripts/Player Scripts/PlayerAnimationManager.cs
@@ -15,12 +15,12 @@ public class PlayerAnimationManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        while(Input.GetKey(KeyCode.LeftArrow))
+        if(Input.GetKey(KeyCode.LeftArrow))
         {
             playerWalking = true;
         }
 
-        while(Input.GetKey(KeyCode.RightArrow))
+        if(Input.GetKey(KeyCode.RightArrow))
         {
             playerWalking = true;
         }
@@ -32,7 +32,7 @@ public class PlayerAnimationManager : MonoBehaviour
     {
         if (playerWalking)
         {
-            playerAnimator.SetBool("isWalking", false);
+            playerAnimator.SetBool("isWalking", true);
             playerWalking = false;
         }
     }
