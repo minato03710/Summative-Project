b8ffa74c8a48701ee2894b4f98eb4fdcfadfa9d2
minato03710 <shimojihana1@gmail.com>
2026-09-21 12:37:04 +1200

    Player animation manager update

--- Assets/Minato/Scripts/Player Scripts/PlayerAnimationManager.cs
+++ Assets/Minato/Scripts/Player Scripts/PlayerAnimationManager.cs
@@ -2,15 +2,55 @@ using UnityEngine;
 
 public class PlayerAnimationManager : MonoBehaviour
 {
+
+    public Animator playerAnimator;
+    private bool playerWalking;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerAnimator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        while(Input.GetKey(KeyCode.LeftArrow))
+        {
+            playerWalking = true;
+        }
+
+        while(Input.GetKey(KeyCode.RightArrow))
+        {
+            playerWalking = true;
+        }
+
+        Jumping();
     }
+
+    void Jumping()
+    {
+        if (playerWalking)
+        {
+            playerAnimator.SetBool("isWalking", false);
+            playerWalking = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            playerAnimator.SetBool("onGround", true);
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            playerAnimator.SetBool("onGround", false);
+        }
+    }
+
 }
